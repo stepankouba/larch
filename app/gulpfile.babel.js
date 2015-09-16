272a90ba7ce3ea4e9a625a967f2dd13f1dc7a6b8
Štěpán Kouba <stepan.kouba@gmail.com>
2015-09-16 14:23:45 +0200

    APP: fixes #15 APP: karma now watching files for tests itself

---
+++ app/gulpfile.babel.js
@@ -0,0 +1,181 @@
+import gulp from 'gulp';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import watch from 'gulp-watch';
+import less from 'gulp-less';
+import concat from 'gulp-concat';
+import connect from 'gulp-connect';
+import util from 'gulp-util';
+import uglify from 'gulp-uglify';
+import karma from 'karma';
+
+
+const BUILD_PATH = './build';
+const SRC_PATH = './src';
+const NODE_MODULES_PATH = './node_modules';
+
+const paths = {
+	build: {
+		default: `${BUILD_PATH}/**/*.*`,
+		js: `${BUILD_PATH}/js`,
+
+		css: `${BUILD_PATH}/css`,
+		index: `${BUILD_PATH}/`,
+		templates: `${BUILD_PATH}/templates`,
+		images: `${BUILD_PATH}/images`,
+		bootstrap: `${BUILD_PATH}/css/bootstrap`,
+		fontawesome: `${BUILD_PATH}/css/font-awesome/css`,
+		fontawesome_fonts: `${BUILD_PATH}/css/font-awesome/fonts`
+	},
+	src: {
+		js: `${SRC_PATH}/js`,
+		less: `${SRC_PATH}/less/**/*.less`,
+		index: `${SRC_PATH}/index.html`,
+		templates: `${SRC_PATH}/templates/**/*.*`,
+		fonts: `${SRC_PATH}/fonts/*.*`,
+		images: `${SRC_PATH}/images/*.*`,
+		tests: './spec/**/*.es6',
+		bootstrap: `${NODE_MODULES_PATH}/bootstrap/dist/**/*.*`,
+		fontawesome: `${NODE_MODULES_PATH}/font-awesome/css/*.*`,
+		fontawesome_fonts: `${NODE_MODULES_PATH}/font-awesome/fonts/*.*`
+	}
+};
+
+gulp.task('app', () => {
+	browserify({
+		entries: `${paths.src.js}/larch.es6`,
+		debug: true
+	})
+		.transform(babelify)
+		.bundle()
+		.on('error', util.log)
+		.pipe(source('larch.app.js'))
+		.pipe(gulp.dest(paths.build.js))
+		;
+});
+
+gulp.task('compress', () => {
+	return gulp.src(`${paths.build.js}/larch.app.js`)
+		.pipe(uglify())
+		.pipe(gulp.dest(`${paths.build.js}/min`));
+});
+
+gulp.task('less', () => {
+	return gulp.src(paths.src.less)
+		.pipe(less())
+		.pipe(concat('larch.css'))
+		.pipe(gulp.dest(paths.build.css));
+});
+
+gulp.task('bootstrap', () => {
+	return gulp.src(paths.src.bootstrap)
+		.pipe(gulp.dest(paths.build.bootstrap));
+});
+
+gulp.task('fontawesome', () => {
+	return gulp.src(paths.src.fontawesome)
+		.pipe(gulp.dest(paths.build.fontawesome));
+});
+
+gulp.task('fontawesome_fonts', () => {
+	return gulp.src(paths.src.fontawesome_fonts)
+		.pipe(gulp.dest(paths.build.fontawesome_fonts));
+});
+
+gulp.task('index', () => {
+	return gulp.src(paths.src.index)
+		.pipe(gulp.dest(paths.build.index));
+});
+
+gulp.task('templates', () => {
+	return gulp.src(paths.src.templates)
+		.pipe(gulp.dest(paths.build.templates));
+});
+
+gulp.task('assets', () => {
+	return gulp.src(paths.src.fonts)
+		.pipe(gulp.dest(paths.build.css));
+});
+
+gulp.task('images', () => {
+	return gulp.src(paths.src.images)
+		.pipe(gulp.dest(paths.build.images));
+});
+
+gulp.task('test', done => {
+	// return gulp.src('./foobar')
+	// 	.pipe(karma({
+	// 		configFile: 'karma.conf.js',
+	// 		action: 'run'
+	// 	}))
+	// 	.on('error', err => {
+	// 		// Make sure failed tests cause gulp to exit non-zero
+	// 		console.log(err);
+	// 		// instead of erroring the stream, end it
+	// 		this.emit('end');
+	// 	});
+	// const server = new karma.Server({port: 9876}, exitCode => {
+	// 	console.log(`Karma has exited with ${exitCode}`);
+	// });
+
+	const server = new karma.Server({configFile: `${__dirname}/karma.conf.js`}, exitCode => {
+		console.log(`Karma has exited with ${exitCode}`);
+		done();
+	});
+	server.start();
+
+	// server.on('browser_register', browser => {
+	// 	karma.runner.run({
+	// 	}, () => done());
+	// });
+});
+
+/**
+ * Watch custom files
+ */
+gulp.task('watch', () => {
+	gulp.watch([`${paths.src.js}/**/*.es6`], ['app']);
+	// gulp.watch([paths.src.tests], ['test']);
+	gulp.watch([paths.src.less], ['less']);
+	gulp.watch([paths.src.fonts], ['assets']);
+	gulp.watch([paths.src.templates], ['templates']);
+	gulp.watch([paths.src.index], ['index']);
+	gulp.watch([paths.src.images], ['images']);
+});
+
+/**
+ * Live reload server
+ */
+gulp.task('webserver', () => {
+	connect.server({
+		root: [__dirname], // https://github.com/AveVlad/gulp-connect/issues/54
+		livereload: true,
+		port: 3333,
+		// middleware proxy for development
+		middleware(connect, opt) {
+			return [
+				function(req, res, next) {
+					// only in case url is following format: /build/dashboard/1
+					// and not in these cases: /build/index.html#/dashboard/1
+					if (req.url.indexOf('build') > -1 && req.url.indexOf('#') === -1 &&
+						!req.url.match(/\..{2,4}$/g)) {
+						req.url = '/build/index.html';
+					}
+
+					next();
+				}
+			];
+		}
+	});
+});
+
+gulp.task('livereload', () => {
+	gulp.src([paths.build.default])
+		.pipe(watch(paths.build.default))
+		.pipe(connect.reload());
+});
+
+// gulp.task('production', ['app', 'compress','less', 'index', 'templates', 'assets', 'images', 'bootstrap', 'fontawesome', 'fontawesome_fonts']);
+gulp.task('build', ['app', 'test', 'less', 'index', 'templates', 'assets', 'images', 'bootstrap', 'fontawesome', 'fontawesome_fonts']);
+gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);
--- app/karma.conf.js
+++ app/karma.conf.js
@@ -7,32 +7,35 @@ module.exports = function(config) {
 		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '',
 
-
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ['browserify', 'jasmine'],
 
-
 		// list of files / patterns to load in the browser
 		files: [
-			'node_modules/angular/angular.js',
-			'node_modules/angular-mocks/angular-mocks.js',
 			'node_modules/babel/node_modules/babel-core/browser-polyfill.js',
-			//'src/js/larch.app.es6',
-			'spec/**/*.es6'
+			// 'src/js/larch.app.es6',
+			{
+				pattern: 'spec/**/*.es6',
+				watched: false,
+				included: true,
+				served: true
+			}
 		],
-
 
 		// list of files to exclude
 		exclude: [],
 
-
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'src/js/**/*.es6': ['browserify'],
-			'spec/**/*.es6': ['browserify']
+			'./src/js/**/*.es6': ['browserify'],
+			'./spec/**/*.es6': ['browserify']
 		},
+
+		// 'babelPreprocessor': {
+		// 	// options go here
+		// },
 
 		browserify: {
 			debug: true,
@@ -44,28 +47,22 @@ module.exports = function(config) {
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['progress'],
 
-
 		// web server port
 		port: 9876,
 
-
 		// enable / disable colors in the output (reporters and logs)
 		colors: true,
-
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
 
-
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: false,
-
+		autoWatch: true,
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['PhantomJS'],
-
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
